ecf771b6b631608bebeea95e8c1d5193c102a3c5
Jerom Verschoote <33222896+jeromverschoote@users.noreply.github.com>
2023-04-23 22:17:03 +0200

    :fire: Remove dead code.

--- src/types/P5CanvasType.ts
+++
@@ -1,7 +0,0 @@
-import type p5 from 'p5';
-
-export type P5CanvasType<
-  Props extends { [key: string]: unknown } = { [key: string]: unknown },
-> = p5 & {
-  updateWithProps?: (props: Props) => void;
-};
